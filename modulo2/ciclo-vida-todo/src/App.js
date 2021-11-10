6eb85607bde3e4759d6acfaa5ba018f2dffa6da4
Rodrigo Calazans Santos <rodrigoocalazans@gmail.com>
2021-11-09 23:29:32 -0300

    Exercicio ciclo vida

---
+++ modulo2/ciclo-vida-todo/src/App.js
@@ -0,0 +1,141 @@
+import React from 'react'
+import styled from 'styled-components'
+import './styles.css'
+
+const TarefaList = styled.ul`
+  padding: 0;
+  width: 200px;
+`
+
+const Tarefa = styled.li`
+  text-align: left;
+  text-decoration: ${({completa}) => (completa ? 'line-through' : 'none')};
+`
+
+const InputsContainer = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  gap: 10px;
+`
+
+class App extends React.Component {
+    state = {
+      tarefas: [
+        {
+          id: Date.now(), 
+	        texto: 'Texto da tarefa',
+	        completa: false
+        },
+        {
+          id: Date.now(), 
+	        texto: 'Texto da segunda tarefa',
+	        completa: true
+        }
+      ],
+      inputValue: '',
+      filtro: ''
+    }
+
+  // saveTarefas = () => {
+  //     localStorage.setItem("tarefas", this.state.tarefas)
+  //   }
+  
+  // buscarTarefas = () => {
+  //     const tarefasStorage = localStorage.getItem("tarefas")
+
+  //     this.setState({
+  //       tarefas: tarefasStorage || ""
+  //     })
+  //   }
+
+  // componentDidUpdate(prevState) {
+  //   if (prevState.tarefas === this.state.tarefas) {
+  //     this.saveTarefas()
+  //   }
+  // };
+
+  // componentDidMount() {
+  //   this.buscarTarefas()
+  // };
+
+  onChangeInput = (event) => {
+    this.setState({ inputValue: event.target.value })
+  }
+
+  criaTarefa = () => {
+    const novaTarefa =
+    {
+      id: Date.now(), 
+	    texto: '',
+	    completa: false
+    }
+    const copiaDoEstado =[...this.state.tarefas, novaTarefa]
+    this.setState({ tarefas: copiaDoEstado})
+  }
+
+  selectTarefa = (id) => {
+    const trocaCompleta = this.state.tarefas.map((objeto)=>{
+      if (id === objeto.id) {
+        const novoId = {
+          ...objeto,
+          id: !objeto.id
+        }
+        return novoId
+      }else {
+        return id
+      }
+    })
+    this.setState({ tarefas: trocaCompleta })
+  }
+
+  onChangeFilter = (event) => {
+    this.setState({ filtro: event.target.value })
+  }
+
+  render() {
+    const listaFiltrada = this.state.tarefas.filter(tarefa => {
+      switch (this.state.filtro) {
+        case 'pendentes':
+          return !tarefa.completa
+        case 'completas':
+          return tarefa.completa
+        default:
+          return true
+      }
+    })
+
+    return (
+      <div className="App">
+        <h1>Lista de tarefas</h1>
+        <InputsContainer>
+          <input value={this.state.inputValue} onChange={this.onChangeInput}/>
+          <button onClick={this.criaTarefa}>Adicionar</button>
+        </InputsContainer>
+        <br/>
+
+        <InputsContainer>
+          <label>Filtro</label>
+          <select value={this.state.filter} onChange={this.onChangeFilter}>
+            <option value="">Nenhum</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="completas">Completas</option>
+          </select>
+        </InputsContainer>
+        <TarefaList>
+          {listaFiltrada.map(tarefa => {
+            return (
+              <Tarefa
+                completa={tarefa.completa}
+                onClick={() => this.selectTarefa(tarefa.id)}
+              >
+                {tarefa.texto}
+              </Tarefa>
+            )
+          })}
+        </TarefaList>
+      </div>
+    )
+  }
+}
+
+export default App
